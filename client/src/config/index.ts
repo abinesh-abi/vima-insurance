5210a15933e5add5b4e20699dae0c039aab0caf0
abinesh-abi <abinesh1729@gmail.com>
2025-03-07 13:43:02 +0530

    fix: react - .env issue

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,5 +1,5 @@
 const CONFIG = {
-  SERVER_URL: process.env.SERVER_URL || 'http://127.0.0.1:5000',
+  SERVER_URL: process.env.REACT_APP_SERVER_URL || 'http://127.0.0.1:5000',
 };
 
 export default CONFIG;
